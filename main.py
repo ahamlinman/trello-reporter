0eca57215ab030ac800c1abb3ed8dde3b2d1fad6
Alex Hamlin <alex@alexhamlin.co>
2017-07-21 22:24:14 -0700

    Add email body formatting
    Add formatting for the email body, so that the email looks nice.

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def is_card_old(card):
     card_date = parse_date(card['dateLastActivity'])
     return datetime.now(tz=card_date.tzinfo) - card_date > timedelta(days=7)
 
+def format_card_email(cards):
+    lines = ['* {}'.format(c['name']) for c in cards]
+    heading = 'Please review the following Trello cards. They are at least ' \
+            'one week old.'
+
+    return '{}\n\n{}'.format(heading, '\n'.join(lines))
+
 trello_key = os.getenv('TRELLO_KEY')
 trello_token = os.getenv('TRELLO_TOKEN')
 trello_list_id = os.getenv('TRELLO_LIST_ID')
@@ -23,4 +30,4 @@ trello_list_id = os.getenv('TRELLO_LIST_ID')
 all_cards = get_cards_from_trello(trello_key, trello_token, trello_list_id)
 old_cards = filter(is_card_old, all_cards)
 
-print(list(old_cards))
+print(format_card_email(old_cards))
