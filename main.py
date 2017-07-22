5b971958c4e569f5c276e206625dff0a1c3c83f1
Alex Hamlin <alex@alexhamlin.co>
2017-07-21 21:22:12 -0700

    Put Trello function back into main
    Just give up on modules because this should really be a pretty small
    script.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-from trelloclient import TrelloClient
+import requests
 import os
+
+def get_cards_from_trello(key, token, list_id):
+    url = 'https://api.trello.com/1/lists/{}/cards'.format(list_id)
+    params = { 'key': key, 'token': token }
+
+    r = requests.get(url, params=params)
+    return r.json()
 
 trello_key = os.getenv('TRELLO_KEY')
 trello_token = os.getenv('TRELLO_TOKEN')
 trello_list_id = os.getenv('TRELLO_LIST_ID')
 
-client = TrelloClient(trello_key, trello_token)
-print(client.get_cards_from_list(trello_list_id))
+print(get_cards_from_list(trello_key, trello_token, trello_list_id))
